2ae3059e1a48e160f76f670222a82bb37998f224
tnechepurencko <321avisam5@gmail.com>
2021-11-05 13:41:53 +0300

    +def open_session +def close_session +restrictions with 'in_session' flag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 
+# @JointChillBot
 bot = telebot.TeleBot('1931778515:AAEDB-WOSq0oV2WCFzZ0NYV2CmXQGRSp8aQ')
 
 
@@ -91,22 +92,80 @@ class ChillSessionsHandler:
 class Communication:
     def __init__(self):
         self.chillSessionsHandler = ChillSessionsHandler()
+        self.in_session = False
+        self.opened_session = None
 
     def new_session(self, message):
-        self.chillSessionsHandler.add_user(message.from_user.id)
-        name_of_session = message.text
-        bot.send_message(message.from_user.id, 'thanks')
-        self.chillSessionsHandler.add_chill_session(message.from_user.id, name_of_session)
+        if self.in_session:
+            bot.send_message(message.from_user.id, 'You cannot use this command inside the session')
+        else:
+            self.chillSessionsHandler.add_user(message.from_user.id)
+            name_of_session = message.text
+            bot.send_message(message.from_user.id, 'thanks')
+            self.chillSessionsHandler.add_chill_session(message.from_user.id, name_of_session)
 
     def delete_session(self, message):
-        name_of_session = message.text
-        if message.from_user.id not in self.chillSessionsHandler.useridToChillSessions.keys():
-            bot.send_message(message.from_user.id, 'this session does not exist')
-        elif not self.chillSessionsHandler.chill_session_exists(message.from_user.id, name_of_session):
-            bot.send_message(message.from_user.id, 'this session does not exist')
+        if self.in_session:
+            bot.send_message(message.from_user.id, 'You cannot use this command inside the session')
         else:
-            self.chillSessionsHandler.delete_chill_session(message.from_user.id, name_of_session)
-            bot.send_message(message.from_user.id, 'thanks')
+            name_of_session = message.text
+            if message.from_user.id not in self.chillSessionsHandler.useridToChillSessions.keys():
+                bot.send_message(message.from_user.id, 'this session does not exist')
+            elif not self.chillSessionsHandler.chill_session_exists(message.from_user.id, name_of_session):
+                bot.send_message(message.from_user.id, 'this session does not exist')
+            else:
+                self.chillSessionsHandler.delete_chill_session(message.from_user.id, name_of_session)
+                bot.send_message(message.from_user.id, 'thanks')
+
+    def set_opened_session(self, session):
+        self.opened_session = session
+
+    def open_session(self, message):
+        if self.in_session:
+            bot.send_message(message.from_user.id, 'You cannot use this command inside the session')
+        else:
+            name_of_session = message.text
+            if message.from_user.id not in self.chillSessionsHandler.useridToChillSessions.keys():
+                bot.send_message(message.from_user.id, 'this session does not exist')
+            elif not self.chillSessionsHandler.chill_session_exists(message.from_user.id, name_of_session):
+                bot.send_message(message.from_user.id, 'this session does not exist')
+            else:
+                ind = self.chillSessionsHandler.chill_session_index(message.from_user.id, name_of_session)
+                self.set_opened_session(self.chillSessionsHandler.useridToChillSessions[message.from_user.id][ind])
+                self.in_session = True
+                bot.send_message(message.from_user.id, 'You are in the session \"' + self.opened_session.name + '\"!')
+
+    def exit_session(self, message):
+        if not self.in_session:
+            bot.send_message(message.from_user.id, 'You are not inside a session')
+        else:
+            self.set_opened_session(None)
+            self.in_session = False
+            bot.send_message(message.from_user.id, 'You have exited the session successfully!')
+
+
+    # def add_expenses_2(self, message, name):
+    #     amount = message.text.split('.')
+    #     if len(amount == 1):
+    #         amount = message.text.split(',')
+    #         if len(amount == 1):
+    #             wrong_format = False
+    #             for i in range(len(amount[0])):
+    #                 if str(amount[0][i]).isdigit():
+    #                     wrong_format = True
+    #                     break
+    #             if wrong_format:
+    #                 bot.send_message(message.from_user.id, 'wrong format! try again!')
+    #             else:
+    #                 if not self.chillSessionsHandler.useridToChillSessions[message.from_user.id][]
+    #
+    #                     person = Person(name)
+    #
+    #
+    # @staticmethod
+    # def add_expenses_1(message):
+    #     name = message.text
+    #     bot.register_next_step_handler(message, communication.add_expenses_2, name)
 
 
 communication = Communication()
@@ -118,7 +177,10 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'list of commands:\n'
                                                '/new_session\n'
                                                '/list_of_sessions\n'
-                                               '/delete_session\n')
+                                               '/delete_session\n'
+                                               '/open_session\n'
+                                               # '/add_expenses\n'
+                         )
     elif message.text == '/new_session':
         bot.send_message(message.from_user.id, 'name:')
         bot.register_next_step_handler(message, communication.new_session)
@@ -138,6 +200,12 @@ def get_text_messages(message):
     elif message.text == '/delete_session':
         bot.send_message(message.from_user.id, 'name:')
         bot.register_next_step_handler(message, communication.delete_session)
+    elif message.text == '/open_session':
+        bot.send_message(message.from_user.id, 'name:')
+        bot.register_next_step_handler(message, communication.open_session)
+    # elif message.text == '/add_expenses':
+    #     bot.send_message(message.from_user.id, 'name:')
+    #     bot.register_next_step_handler(message, communication.add_expenses_1)
     else:
         bot.send_message(message.from_user.id, 'try \'/help\'')
 
